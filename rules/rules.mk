81821b7a809ab8f7455be36e7e0fa717df904c1b
Mikael Magnusson <mikma@users.sourceforge.net>
2010-07-22 13:54:54 +0200

    Rel and app files depend on config.status
    Since they use @VERSION@.

--- rules/rules.mk
+++ rules/rules.mk
@@ -3,14 +3,14 @@ SUBST = sed $(ERLANG_LIB_VER_SUBST) \
 	-e 's,[@]ERLANG_ERTS_VER[@],$(ERLANG_ERTS_VER),g' \
 	-e 's/[@]$(OTP_APP)_VSN[@]/$($(OTP_APP)_VSN)/'
 
-%.rel: %.rel.in
+%.rel: %.rel.in $(top_builddir)/config.status
 	$(SUBST) $< > $@
 
 %.beam: %.erl
 	@echo [ERLC] $@
 	@$(ERLC) $(AM_ERL_FLAGS) $(ERL_FLAGS) $(AM_ERLCFLAGS) $(ERLCFLAGS) $<
 
-%.app: %.app-in
+%.app: %.app-in $(top_builddir)/config.status
 	$(SUBST) $< > $@
 
 %.boot: %.rel
